5d7dc6937f6970b3c72df25db1a4990708b64894
Jeramy Harrison <dbralir@gmail.com>
2013-12-08 10:21:09 -0500

    Added ICPC 2011 - Refrigerator Magnets solution.

---
+++ icpc/na-mid-central/2011/magnets.cpp
@@ -0,0 +1,34 @@
+#include <fstream>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+// Determines if a string contains no duplicate letters.
+bool is_valid(const string& str)
+{
+    // A set might not be the fastest way of solving this, but it's the easiest.
+    set<char> letters;
+    
+    for (unsigned i=0, ie=str.size(); i!=ie; ++i)
+    {
+        if (str[i]!=' ')
+        {
+            if (!letters.insert(str[i]).second) return false;
+        }
+    }
+    
+    return true;
+}
+
+int main()
+{
+    ifstream in ("magnets.in");
+    
+    string line;
+    
+    while (getline(in, line), line != "END")
+    {
+        if (is_valid(line)) cout <<line <<endl;
+    }
+}
